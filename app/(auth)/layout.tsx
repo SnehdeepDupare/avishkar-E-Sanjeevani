f07f5450229366b3ef160430d52315cc3954ab2d
Snehdeep <duparesnehdeep@gmail.com>
2023-12-15 02:31:51 +0530

    style: Added Logo in the auth page

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "../globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
+import Image from "next/image";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -27,7 +28,21 @@ export default function RootLayout({
         <body
           className={`${inter.className} bg-[#f1f1f1] h-screen flex items-center justify-center`}
         >
-          {children}
+          <main className="flex flex-col items-center ">
+            <div className="flex items-center gap-4 ">
+              <div className="relative h-28 w-28">
+                <Image
+                  src="/assets/logo.png"
+                  alt="logo"
+                  fill
+                  priority
+                  className="object-contain"
+                />
+              </div>
+              <h2 className="font-bold text-2xl">E-Sanjeevani</h2>
+            </div>
+            {children}
+          </main>
         </body>
       </html>
     </ClerkProvider>
